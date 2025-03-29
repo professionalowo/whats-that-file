71c43fb0e3b1746f0a2f7b23c3a8e53468f161fa
professionalowo <martin.brandl1307@gmail.com>
2025-03-29 22:22:57 +0100

    implement getInArgAlloc so that arg is the last argument passed

--- src/arguments.zig
+++ src/arguments.zig
@@ -9,7 +9,7 @@ const readlineAlloc = @import("readline").readlineAlloc;
 pub fn getInArgAlloc(allocator: Allocator) !?[]const u8 {
     const args = try std.process.argsAlloc(allocator);
     if (args.len >= 2) {
-        return args[1];
+        return args[args.len - 1];
     }
     return try readlineAlloc(allocator, "Path: ");
 }
