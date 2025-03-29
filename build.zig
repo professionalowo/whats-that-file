22c58cb6c711e43e68296520978a42e976c9a605
professionalowo <martin.brandl1307@gmail.com>
2025-03-29 21:41:29 +0100

    link libc

--- build.zig
+++ build.zig
@@ -66,6 +66,7 @@ pub fn build(b: *std.Build) void {
     });
 
     exe.linkSystemLibrary("readline");
+    exe.linkLibC();
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
