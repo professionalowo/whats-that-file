7a169c2ad7cdd0f066c2f023f9726431f4ef82ff
professionalowo <martin.brandl1307@gmail.com>
2025-03-29 21:54:47 +0100

    set readline completion character to none

--- src/readline.zig
+++ src/readline.zig
@@ -2,10 +2,15 @@ const std = @import("std");
 const mem = std.mem;
 const Allocator = mem.Allocator;
 
+extern var rl_completion_append_character: c_int;
+
 extern fn readline(prompt: [*:0]const u8) callconv(.C) ?[*:0]u8;
 extern fn free(ptr: ?*anyopaque) callconv(.C) void;
 
 pub fn readlineAlloc(allocator: Allocator, comptime prompt: [:0]const u8) !?[]const u8 {
+    if (rl_completion_append_character != 0) {
+        rl_completion_append_character = 0;
+    }
     const line = readline(prompt) orelse return null;
     defer free(line);
 
