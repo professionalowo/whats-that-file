54f8f7663efd1f69aeecca4655a802eb7db083a3
professionalowo <martin.brandl1307@gmail.com>
2025-03-29 22:22:01 +0100

    more than one arg are ok

--- src/arguments.zig
+++ src/arguments.zig
@@ -8,7 +8,7 @@ const readlineAlloc = @import("readline").readlineAlloc;
 /// provided. The argument is allocated using the provided `allocator`.
 pub fn getInArgAlloc(allocator: Allocator) !?[]const u8 {
     const args = try std.process.argsAlloc(allocator);
-    if (args.len == 2) {
+    if (args.len >= 2) {
         return args[1];
     }
     return try readlineAlloc(allocator, "Path: ");
