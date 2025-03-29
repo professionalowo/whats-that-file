c509dcfa8da5583f15563f150c240e88eabd3f47
professionalowo <martin.brandl1307@gmail.com>
2025-03-29 21:45:42 +0100

    renamed function

--- src/readline.zig
+++ src/readline.zig
@@ -5,7 +5,7 @@ const Allocator = mem.Allocator;
 extern fn readline(prompt: [*:0]const u8) callconv(.C) ?[*:0]u8;
 extern fn free(ptr: ?*anyopaque) callconv(.C) void;
 
-pub fn readLineAlloc(allocator: Allocator, comptime prompt: [:0]const u8) !?[]const u8 {
+pub fn readlineAlloc(allocator: Allocator, comptime prompt: [:0]const u8) !?[]const u8 {
     const line = readline(prompt) orelse return null;
     defer free(line);
 
