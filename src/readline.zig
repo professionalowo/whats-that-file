6817b67818ba0c2c568842754fddf97b2a5e7aca
professionalowo <martin.brandl1307@gmail.com>
2025-03-29 22:06:59 +0100

    removed decl of Allocator

--- src/readline.zig
+++ src/readline.zig
@@ -1,12 +1,11 @@
 const mem = @import("std").mem;
-const Allocator = mem.Allocator;
 
 extern var rl_completion_append_character: c_int;
 
 extern fn readline(prompt: [*:0]const u8) callconv(.C) ?[*:0]u8;
 extern fn free(ptr: ?*anyopaque) callconv(.C) void;
 
-pub fn readlineAlloc(allocator: Allocator, comptime prompt: [:0]const u8) !?[]const u8 {
+pub fn readlineAlloc(allocator: mem.Allocator, comptime prompt: [:0]const u8) !?[]const u8 {
     if (rl_completion_append_character != 0) {
         rl_completion_append_character = 0;
     }
