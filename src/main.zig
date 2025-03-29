4953c8baf60e38c9197afb4c147229f0583b0515
professionalowo <martin.brandl1307@gmail.com>
2025-03-29 12:41:40 +0100

    added fn errexit

--- src/main.zig
+++ src/main.zig
@@ -2,26 +2,25 @@ const std = @import("std");
 const args = @import("arguments");
 const paths = @import("paths");
 
-pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-    const stderr = std.io.getStdOut().writer();
+const stdout = std.io.getStdOut().writer();
+const stderr = std.io.getStdOut().writer();
 
+pub fn main() !void {
     var buffer: [std.fs.max_name_bytes * 16]u8 = undefined;
     var fba = std.heap.FixedBufferAllocator.init(&buffer);
 
     const allocator = fba.allocator();
 
-    const inArg = try args.getInArgAlloc(allocator) orelse {
-        try stderr.print("please provide at least one argument", .{});
-        defer std.process.exit(1);
-    };
+    const inArg = try args.getInArgAlloc(allocator) orelse errexit("please provide at least one argument");
     defer allocator.free(inArg);
 
-    const realPath = paths.getRealPathAlloc(allocator, inArg) catch {
-        try stderr.print("please provide a valid filepath", .{});
-        defer std.process.exit(1);
-    };
+    const realPath = paths.getRealPathAlloc(allocator, inArg) catch errexit("please provide a valid filepath");
     defer allocator.free(realPath);
 
     try stdout.print("{s}", .{realPath});
+}
+
+fn errexit(comptime message: []const u8) noreturn {
+    defer std.process.exit(1);
+    try stderr.print(message, .{});
 }
