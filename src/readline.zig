458dc933df23a1c0a2f59199dc5dd2cf2f422f1e
professionalowo <martin.brandl1307@gmail.com>
2025-03-29 22:35:46 +0100

    properly documented readlineAlloc

--- src/readline.zig
+++ src/readline.zig
@@ -5,6 +5,9 @@ extern var rl_completion_append_character: c_int;
 extern fn readline(prompt: [*:0]const u8) callconv(.C) ?[*:0]u8;
 extern fn free(ptr: ?*anyopaque) callconv(.C) void;
 
+/// Reads a line from stdin using the GNU Readline library.
+/// The line is allocated using the provided `allocator`.
+/// The caller has to free the returned slice
 pub fn readlineAlloc(allocator: mem.Allocator, comptime prompt: [:0]const u8) !?[]const u8 {
     if (rl_completion_append_character != 0) {
         rl_completion_append_character = 0;
