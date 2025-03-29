282141cf6d7a2e9827e941cedbd64ff2d0d413a3
professionalowo <martin.brandl1307@gmail.com>
2025-03-29 12:02:55 +0100

    swapped out gpa for fba

--- src/main.zig
+++ src/main.zig
@@ -5,23 +5,16 @@ pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     const stderr = std.io.getStdOut().writer();
 
-    var gpa = std.heap.GeneralPurposeAllocator(.{ .thread_safe = false }).init;
-    defer switch (gpa.deinit()) {
-        .leak => @panic("allocator leaked memory"),
-        else => {},
-    };
+    var buffer: [std.fs.max_name_bytes * 16]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buffer);
 
-    const parentAllocator = gpa.allocator();
-
-    var arena = std.heap.ArenaAllocator.init(parentAllocator);
-    defer arena.deinit();
-
-    const allocator = arena.allocator();
+    const allocator = fba.allocator();
 
     const realPath = args.getRealPathOfInArg(allocator) catch {
         try stderr.print("please provide a valid filepath", .{});
         defer std.process.exit(1);
     };
+    defer allocator.free(realPath);
 
     try stdout.print("{s}", .{realPath});
 }
